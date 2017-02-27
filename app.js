f1664fda74a89ff91adda946006e6ff87dc1e709
r48patel <r48patel@gmail.com>
2017-02-27 10:02:42 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.redirect('/index');
+});
+
+var bio_info_map = {}
+fs.readdirSync(path.join(__dirname , 'views','templates')).forEach(file => {
+    name = file.split('.')[0].split('_')[1]
+    bio_info_map[name] = get_bio_info(name)
+});
+global.bio_info_map = bio_info_map;
+
+function get_bio_info(name){
+    file_name = 'bio_' + name + '.txt'
+    bio_info = {}
+
+    var bio_file = fs.readFileSync(path.join(__dirname , 'views','templates', file_name)).toString().split('\n');
+    bio_info['name'] = bio_file[0].split(',')[0].trim()
+    bio_info['position'] = bio_file[0].split(',')[1].trim()
+    bio_info['img'] = bio_file[1]
+    var bio_qna = []
+    for(var i = 2; i < bio_file.length;){
+        bio_qna.push({question: bio_file[i], answer: bio_file[i+1]})
+        i = i +2
+    }
+    bio_info['qna'] = bio_qna
+
+    return bio_info
+}
+
+app.get('/bio-:name', function(request, response) {
+    req_name = request.params.name
+
+    response.render('pages/bio', {
+        bio_info: bio_info_map[req_name]
+    });
+});
+
+
+app.get('/:type', function(request, response) {
+    reqType = request.params.type
+    response.render('pages/'+reqType, {});
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
