d5faf435ac6949990c4f3165757ab761bf5d072a
r48patel <r48patel@gmail.com>
2017-02-28 10:59:55 -0600

    Adding images and training page

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ var path = require('path');
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname , 'public')));
 
 // views is directory for all template files
-// app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname , 'views'));
+app.set('public', path.join(__dirname , 'public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
@@ -17,17 +18,27 @@ app.get('/', function(request, response) {
 });
 
 var bio_info_map = {}
-fs.readdirSync(path.join(__dirname , 'views','templates')).forEach(file => {
+fs.readdirSync(path.join('views','templates')).forEach(file => {
     name = file.split('.')[0].split('_')[1]
     bio_info_map[name] = get_bio_info(name)
 });
+
+var gallery_list = []
+fs.readdirSync(path.join('public', 'images', 'gallery')).forEach(file => {
+    if(!file.startsWith('.')){
+        gallery_list.push(file)
+    }
+});
+
 global.bio_info_map = bio_info_map;
+global.gallery_list = gallery_list;
+
 
 function get_bio_info(name){
     file_name = 'bio_' + name + '.txt'
     bio_info = {}
 
-    var bio_file = fs.readFileSync(path.join(__dirname , 'views','templates', file_name)).toString().split('\n');
+    var bio_file = fs.readFileSync(path.join('views','templates', file_name)).toString().split('\n');
     bio_info['name'] = bio_file[0].split(',')[0].trim()
     bio_info['position'] = bio_file[0].split(',')[1].trim()
     bio_info['img'] = bio_file[1]
@@ -44,7 +55,7 @@ function get_bio_info(name){
 app.get('/bio-:name', function(request, response) {
     req_name = request.params.name
 
-    response.render('pages/bio', {
+    response.render(path.join('pages','bio'), {
         bio_info: bio_info_map[req_name]
     });
 });
@@ -52,7 +63,7 @@ app.get('/bio-:name', function(request, response) {
 
 app.get('/:type', function(request, response) {
     reqType = request.params.type
-    response.render('pages/'+reqType, {});
+    response.render(path.join('pages', reqType), {});
 });
 
 app.listen(app.get('port'), function() {
